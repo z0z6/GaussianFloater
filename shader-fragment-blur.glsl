b62c4c5f4f9858fc8aee9aa10fd2939de55146fa
z0z6 <elbrujo2020@protonmail.com>
2026-08-28 14:33:05 +0200

    Create shader-fragment-blur.glsl

---
+++ shader-fragment-blur.glsl
@@ -0,0 +1,34 @@
+precision highp float;
+
+uniform sampler2D u_texture;
+uniform vec2 u_texel; // 1.0 / rozmiar tekstury
+uniform float u_sigma;
+uniform bool u_horizontal;
+
+varying vec2 v_uv;
+
+float gaussian(float x, float sigma) {
+  return exp(-(x * x) / (2.0 * sigma * sigma));
+}
+
+void main() {
+  const int samples = 7;
+  const float radius = 3.0;
+
+  vec3 color = vec3(0.0);
+  float totalWeight = 0.0;
+
+  for (int i = -samples; i <= samples; i++) {
+    float x = float(i) / float(samples);
+    float w = gaussian(x * radius, u_sigma);
+
+    vec2 offset = u_horizontal
+      ? vec2(x * u_texel.x, 0.0)
+      : vec2(0.0, x * u_texel.y);
+
+    color += w * texture2D(u_texture, v_uv + offset).rgb;
+    totalWeight += w;
+  }
+
+  gl_FragColor = vec4(color / totalWeight, 1.0);
+}
