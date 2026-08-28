d509d78a6ace3c623d7a3ee3b4f22f980a56b1cb
z0z6 <elbrujo2020@protonmail.com>
2026-08-28 14:43:24 +0200

    Update webgl-helpers.js

--- webgl-helpers.js
+++ webgl-helpers.js
@@ -58,6 +58,24 @@ export function createTexture(gl, width, height) {
   return tex;
 }
 
+export function createTextureFromImage(gl, image) {
+  const tex = gl.createTexture();
+  gl.bindTexture(gl.TEXTURE_2D, tex);
+  gl.texImage2D(
+    gl.TEXTURE_2D,
+    0,
+    gl.RGBA,
+    gl.RGBA,
+    gl.UNSIGNED_BYTE,
+    image
+  );
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+  return tex;
+}
+
 export function createFramebuffer(gl, texture) {
   const fb = gl.createFramebuffer();
   gl.bindFramebuffer(gl.FRAMEBUFFER, fb);
